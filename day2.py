8b4d1e13e47daa4792946d5526edaa2ea5dab0a8
ruhanul29 <154474217+ruhanul29@users.noreply.github.com>
2025-12-16 23:44:58 +0600

    Add files via upload
    Task-Day2

---
+++ day2.py
@@ -0,0 +1,30 @@
+#micro-1
+
+user = None
+if user is not None and user.is_admin:
+    pass
+
+#micro-2
+
+a =0.1
+b =0.2
+c = 0.3
+if (a+b)==c:
+    print("True") 
+else:
+    print("False")
+    
+
+#micro-3
+
+data =[]
+if data:
+    print("Data Found")
+else:
+    print("No Data")    
+
+#micro-4
+
+score = 85
+status = "Pass" if score > 70 else "Fail"
+print(status)
